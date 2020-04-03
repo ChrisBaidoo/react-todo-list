f0f2cb223557382f4feda9f8e7f31dac6f4e5e04
christian baidoo <chris_pens@hotmail.co.uk>
2020-04-03 05:27:43 +0100

    Created Todo and TodoItem components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos/>
+
+ class App extends Component {
+
+  state = {
+    todos :[
+      {
+        id: 1,
+        title: "Wash dishes",
+        completed: "false"
+      },
+      {
+        id: 2,
+        title: "Do abs",
+        completed: "false"
+      },
+
+      {
+        id: 3,
+        title: "Read book", 
+        completed: "false"
+      },
+    ]
+  }
+
+    render() {
+      console.log(this.state.todos)
+
+      return(
+      <div className="App">
+      <Todos todos ={this.state.todos}/>
     </div>
-  );
+      )
+        
+    }
 }
+
+
 
 export default App;
